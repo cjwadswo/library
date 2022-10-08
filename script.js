47a020ff31cad6c7abbdc82c05f5fe4ef18086f3
Chris Wadsworth <cjwadswo@uno.edu>
2022-10-08 00:24:08 -0500

    Add a toggle switch to toggle if the book has been read or not

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 let myLibrary = [];
 
-myLibrary.push(new Book("Game of Thrones", "George R. R. Martin", 694, true));
-myLibrary.push(new Book("Game of Thrones", "George R. R. Martin", 694, true));
-myLibrary.push(new Book("Game of Thrones", "George R. R. Martin", 694, true));
-myLibrary.push(new Book("Game of Thrones", "George R. R. Martin", 694, true));
-myLibrary.push(new Book("Game of Thrones", "George R. R. Martin", 694, true));
+let newBookButton = document.querySelector(".new-book-card");
+newBookButton.addEventListener("click", openForm,true);
+
+let closeButton = document.querySelector(".cancel");
+closeButton.addEventListener("click", closeForm);
+
+let submitButton = document.querySelector("#submit-btn");
+submitButton.addEventListener('click', getNewEntry, true); 
+
+let toggleReadButton = document.querySelector('.read');
+toggleReadButton.addEventListener('click', e => {
+    console.log(e);
+}, true);
+
+// myLibrary.push(new Book("Game of Thrones", "George R. R. Martin", 694, false));
+// myLibrary.push(new Book("Game of Thrones", "George R. R. Martin", 694, true));
+// myLibrary.push(new Book("Game of Thrones", "George R. R. Martin", 694, true));
+// myLibrary.push(new Book("Game of Thrones", "George R. R. Martin", 694, false));
+// myLibrary.push(new Book("Game of Thrones", "George R. R. Martin", 694, true));
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -17,78 +31,99 @@ function displayBooks(library) {
     const content = document.querySelector('.content');
     content.innerHTML = "";
     for(let i = 0; i < myLibrary.length; i++){
-        content.innerHTML += createBookHTML(library[i]);
+        content.innerHTML += createBookHTML(library[i], i);
     }
     content.innerHTML += `            
-    <div class="new-book-card open-button">
+<div class="new-book-card open-button">
     <div class="open-button"> + </div>
-    <div class="form-popup" id="myForm">
-        <form class="form-container">                 
-          <div class="title-field">
-              <label for="title">Title:</label>
-              <input type="text" id="title" placeholder="Enter Book Title" name="title" required>
-          </div>
-      
-          <div class="author-field">
-              <label for="author">Author:</label>
-              <input type="text" id="author" placeholder="Enter Author" name="author" required>
-          </div>
+        <div class="form-popup" id="myForm">
+            <form class="form-container">                 
+            <div class="title-field">
+                <label for="title">Title:</label>
+                <input type="text" id="title" placeholder="Enter Book Title" name="title" required>
+            </div>
+        
+            <div class="author-field">
+                <label for="author">Author:</label>
+                <input type="text" id="author" placeholder="Enter Author" name="author" required>
+            </div>
 
-          <div class="page-field">
-              <label for="pages">Page numbers</label>
-              <input type="number" id="pages" placeholder="#" name="pages" required>
-          </div>
-          <button type="button" id="submit-btn" class="btn testBtn">Add Book</button>
-          <button type="button" class="btn cancel">Close</button>
-        </form>
-      </div>
-</div>`;
+            <div class="page-field">
+                <label for="pages">Page numbers</label>
+                <input type="number" id="pages" placeholder="#" name="pages" required>
+            </div>
+
+            <div class="read-field">
+                <div class="switch-container">
+                    <div>Read</div>
+                    <label class="switch">
+                        <input class="read" type="checkbox">
+                        <span class="slider round"></span>
+                    </label>
+                </div>
+            </div>
+            <button type="button" id="submit-btn" class="btn testBtn">Add Book</button>
+            <button type="button" class="btn cancel">Close</button>
+            </form>
+        </div>
+    </div>`;
+
 newBookButton = document.querySelector(".new-book-card");
 newBookButton.addEventListener("click", openForm, true);
 closeButton = document.querySelector(".cancel");
 closeButton.addEventListener("click", closeForm);
 submitButton = document.querySelector("#submit-btn");
 submitButton.addEventListener('click', getNewEntry, true); 
-
+toggleReadButton = document.querySelectorAll('.read');
+toggleReadButton.forEach((btn)=>{
+    btn.addEventListener('click', e => {
+        console.log(e.path[6]);
+        if(e.path[6].className !== 'new-book-card open-button' ){
+            let bookElement = e.path[3];
+            let index = bookElement.getAttribute('data-book-number');
+            console.log(e.path[6].className);
+            myLibrary[index].read = !myLibrary[index].read;
+        }
+       
+    });
+})
 }
 
-function createBookHTML(test) {
+function createBookHTML(book, i) {
     let bookHTML = `
-    <div class="book-card">
-        <div class="title">${test.title}</div>
-        <div class="author">${test.author}</div>
-        <div class="pages">${test.pages}</div>
-        <div class="read">${test.read}</div>
-    </div>`
+        <div class="book-card" data-book-number=${i}>
+            <div class="title">${book.title}</div>
+            <div class="author">${book.author}</div>
+            <div class="pages">${book.pages}</div>
+            <div class="switch-container">
+                <div>Read</div>
+                <label class="switch">
+                    <input class="read" type="checkbox"${book.read ? 'checked':''}>
+                    <span class="slider round"></span>
+                </label>
+            </div>
+        </div>`;
 
     return bookHTML;
 }
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
-    console.log("TEST");
   }
   
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
   }
 
-
-
-  const submitBtn = document.getElementById("submit-btn");
-
-  function getNewEntry() {
+function getNewEntry() {
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let pages = document.getElementById("pages").value;
-    myLibrary.push(new Book(title,author,pages));
+    let read = document.querySelectorAll(".read");
+    console.log(read[read.length - 1].checked);
+    read = read[read.length - 1].checked;
+    myLibrary.push(new Book(title,author,pages,read));
     displayBooks(myLibrary);
   }
 
-  let newBookButton = document.querySelector(".new-book-card");
-  newBookButton.addEventListener("click", openForm);
-  let closeButton = document.querySelector(".cancel");
-  closeButton.addEventListener("click", closeForm);
 
-  let submitButton = document.querySelector("#submit-btn");
-  submitButton.addEventListener('click', getNewEntry, true); 
